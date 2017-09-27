6c78c9b8372bc98ac2495fb04ff87d862200dd07
Guilherme Alves <guilherme.alves@insidesistemas.com.br>
2017-09-27 16:52:07 -0300

    Novo layout

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(window).load(function () {
+    $('#wrapper').addClass('loaded');
+});
+$('.more-info').click(function () {
+    $("#card").toggleClass('flip');
+    $('#arrow').remove();
+});
+$('#background').click(function () {
+    $('#card').removeClass('flip');
+})
